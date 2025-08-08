f0891f8869ca3f9d5be6125f07aff49a2bfc9cf8
Ronddaa <tronddaa@gmail.com>
2025-08-08 22:00:38 +0200

    add heroMobile v

--- src/components/Hero/HeroMobile/HeroMobile.jsx
+++ src/components/Hero/HeroMobile/HeroMobile.jsx
@@ -1,9 +1,45 @@
-import styles from './HeroMobile.module.css'
+import styles from "./HeroMobile.module.css";
+import { useTranslation } from "react-i18next";
+import heroMainIMG from "../heroMainIMG.webp";
+import heroLavandaIMG from '../heroLavandaIMG.webp'
 
 export default function HeroMobile() {
-    return (
-        <section className={styles.HeroMobile}>
-            
-        </section>
-    )
+  const { t } = useTranslation();
+
+  return (
+    <section className={styles.HeroMobile}>
+      <div className="container">
+        <h1 className={styles.mainTitle}>{t("hero.mainTitle")}</h1>
+        <p className={styles.mainText}>{t("hero.mainText")}</p>
+        <p className={styles.mainTextscnd}>{t("hero.mainTextscnd")}</p>
+        <button className={styles.heroBtn}>{t("hero.heroBtn")}</button>
+      </div>
+      <div className={styles.wrapperIMGhero}>
+        <img
+          className={styles.mainLavandaIMG}
+          src={heroMainIMG}
+          alt="lavanda"
+        />
+        <div className={styles.wrapperCircleHeroText}>
+          <p className={styles.mainTextInCircle}>{t("hero.textInCircle")}</p>
+        </div>
+      </div>
+      <div className="container">
+        <p className={`${styles.mainText} ${styles.maintextLowWidth}`}>
+          {t("hero.heroText1")}
+        </p>
+        <img
+          className={styles.heroLavandaIMG}
+          src={heroLavandaIMG}
+          alt="lavandaOil"
+        />
+        <p className={`${styles.mainTextscnd} ${styles.maintextLowWidth}`}>
+          {t("hero.heroText2")}
+        </p>
+        <p className={`${styles.mainText} ${styles.maintextLowWidth}`}>
+          {t("hero.heroText3")}
+        </p>
+      </div>
+    </section>
+  );
 }
